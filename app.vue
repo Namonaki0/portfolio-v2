46835074daf00a0e527653ff6e9c299dd92f7fcb
Andre <andreferreira@live.co.uk>
2023-06-28 10:48:48 +0100

    components and layouts implemented

--- app.vue
+++ app.vue
@@ -1,5 +1,11 @@
+<script setup></script>
+
 <template>
-  <div>
+  <div class="flex flex-col min-h-screen">
     <SiteHeader />
+    <main class="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8 grow w-full">
+      <NuxtPage />
+    </main>
+    <SiteFooter />
   </div>
 </template>
---
+++ pages/index.vue
@@ -0,0 +1,10 @@
+<script setup></script>
+
+<template>
+  <section>
+    <h1>Hi, welcome to my page</h1>
+  </section>
+  <section>
+    <p></p>
+  </section>
+</template>
